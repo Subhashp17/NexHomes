6c8ad720b7a5a1768e54e6c957209fe07dbf8e50
tidalcurrent <sidneysteward@gmail.com>
2024-04-29 22:16:33 -0400

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,11 +11,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
+      <li><a href="/dashboard">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/services">Services</a></li>
         <li><a href="/contact">Contact</a></li>
-
-        <li><a href="/dashboard">Home</a></li>
         <li><a href = "/" onClick={handleLogout}>Logout</a></li>
 
       </ul>
